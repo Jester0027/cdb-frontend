1b2062743ebf608830eb1416017b6383c0ddc6a3
Jester0027 <Paul.NED@outlook.com>
2020-04-15 19:20:37 +0200

    Animals components added to front module - the not found component has also been added

--- src/app/front/front.module.ts
+++ src/app/front/front.module.ts
@@ -1,9 +1,10 @@
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { WelcomeComponent } from './components/home/welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from './../shared/shared.module';
+import { WelcomeComponent } from './components/home/welcome/welcome.component';
 import { FrontRoutingModule } from './front-routing.module';
 import { FrontComponent } from './front.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -15,7 +16,13 @@ import { HistoryComponent } from './components/home/history/history.component';
 import { ServicesBlockComponent } from './components/home/services-block/services-block.component';
 import { MoreInfoComponent } from './components/home/more-info/more-info.component';
 import { AnimalsSectionComponent } from './components/animals/animals-section/animals-section.component';
-import { AnimalsCardComponent } from './components/animals/animals-card/animals-card.component';
+import { AnimalCardComponent } from './components/animals/animal-card/animal-card.component';
+import { AnimalsComponent } from './pages/animals/animals.component';
+import { AnimalDetailComponent } from './components/animals/animal-detail/animal-detail.component';
+import { AnimalDetailPageComponent } from './pages/animal-detail-page/animal-detail-page.component';
+import { AnimalPaginationComponent } from './components/animals/animal-pagination/animal-pagination.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
@@ -30,8 +37,14 @@ import { AnimalsCardComponent } from './components/animals/animals-card/animals-
     ServicesBlockComponent,
     MoreInfoComponent,
     AnimalsSectionComponent,
-    AnimalsCardComponent
+    AnimalsComponent,
+    AnimalCardComponent,
+    AnimalDetailComponent,
+    AnimalDetailPageComponent,
+    AnimalPaginationComponent,
+    NotFoundComponent,
+    NotFoundPageComponent
   ],
-  imports: [CommonModule, FrontRoutingModule, FontAwesomeModule, ReactiveFormsModule]
+  imports: [CommonModule, FrontRoutingModule, FontAwesomeModule, ReactiveFormsModule, SharedModule]
 })
 export class FrontModule {}
