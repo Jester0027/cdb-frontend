56a2858a3340571df27e850134a45bc72b871e06
Jester0027 <Paul.NED@outlook.com>
2020-04-15 19:15:04 +0200

    form autocompletion - form subject is autocompleted if the user comes from an animal page

--- src/app/front/components/contact-form/contact-form.component.ts
+++ src/app/front/components/contact-form/contact-form.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
@@ -7,21 +8,28 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  form = new FormGroup({
-    email: new FormControl(''),
-    subject: new FormControl(''),
-    content: new FormControl('')
-  });
+  form: FormGroup;
+  subject: string = null;
+  id: string = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params.animal) {
+        this.subject = `[INFO] ${params.animal}`;
+        this.id = params.id;
+      }
+    });
+
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      subject: new FormControl({value: this.subject, disabled: this.subject}),
+      content: new FormControl('')
+    });
   }
 
   onSubmit() {
-    // this.form.controls.forEach(element => {
-    //   console.log(element.value);
-    // });
     // tslint:disable-next-line: forin
     for (const control in this.form.value) {
       console.log(`${control} : ${this.form.value[control]}`);
