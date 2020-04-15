94367cda7c2feffeda2e8860b1683e788718a630
Jester0027 <Paul.NED@outlook.com>
2020-04-15 19:21:30 +0200

    Front routing animals update - animals page, animal detail page - not found page

--- src/app/front/front-routing.module.ts
+++ src/app/front/front-routing.module.ts
@@ -1,7 +1,10 @@
-import { ContactComponent } from './pages/contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { AnimalDetailPageComponent } from './pages/animal-detail-page/animal-detail-page.component';
+import { AnimalsComponent } from './pages/animals/animals.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { FrontComponent } from './front.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -11,13 +14,19 @@ const routes: Routes = [
     component: FrontComponent,
     children: [
       { path: '', component: HomeComponent },
-      { path: 'contact', component: ContactComponent }
-    ]
-  }
+      { path: 'contact', component: ContactComponent },
+      {
+        path: 'animaux',
+        component: AnimalsComponent,
+      },
+      { path: 'animaux/:id', component: AnimalDetailPageComponent },
+      { path: '**', component: NotFoundPageComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class FrontRoutingModule {}
