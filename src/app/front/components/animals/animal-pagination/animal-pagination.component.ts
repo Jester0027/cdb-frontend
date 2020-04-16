f1248f360a6fe1d1ede1871b84a23837dd471480
Jester0027 <Paul.NED@outlook.com>
2020-04-16 17:51:06 +0200

    Animal pagination update - added page number with active page - added Icons

--- src/app/front/components/animals/animal-pagination/animal-pagination.component.ts
+++ src/app/front/components/animals/animal-pagination/animal-pagination.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 import { Meta } from './../../../../models/paginated-data.model';
 
 @Component({
   selector: 'app-animal-pagination',
   templateUrl: './animal-pagination.component.html',
-  styleUrls: ['./animal-pagination.component.scss']
+  styleUrls: ['./animal-pagination.component.scss'],
 })
 export class AnimalPaginationComponent implements OnInit {
   @Input() meta: Meta;
@@ -13,16 +17,25 @@ export class AnimalPaginationComponent implements OnInit {
   prevPage: number;
   nextPage: number;
 
-  constructor() { }
+  links: { pageNumber: number; query: {} }[] = [];
+
+  leftIcon = faAngleDoubleLeft;
+  rightIcon = faAngleDoubleRight;
+
+  constructor() {}
 
   ngOnInit(): void {
     this.prevPage = this.meta.currentPage - 1;
     this.nextPage = this.meta.currentPage + 1;
     console.log(this.meta);
+    for (let i = 1; i <= this.meta.maxPages; i++) {
+      this.links.push({ pageNumber: i, query: { page: i } });
+    }
+
+    console.log(this.links);
   }
 
   changingPage() {
     this.changePage.emit();
   }
-
 }
