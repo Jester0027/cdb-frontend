e2bf58d01dd7c74333fa76ae7acf0239325ab205
Jester0027 <Paul.NED@outlook.com>
2020-04-23 12:27:57 +0200

    Contact form animal info update - contact form displays an error if the id doesn't match an animal - the user can go back on the previous animal page

--- src/app/front/components/contact-form/contact-form.component.ts
+++ src/app/front/components/contact-form/contact-form.component.ts
@@ -1,38 +1,95 @@
-import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { faArrowCircleLeft, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+import { AnimalsService } from './../../../services/animals.service';
+import { Animal } from './../../../models/animals/animal.model';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  styleUrls: ['./contact-form.component.scss'],
 })
-export class ContactFormComponent implements OnInit {
-  form: FormGroup;
+export class ContactFormComponent implements OnInit, OnDestroy {
+  private routeSub: Subscription;
+  private animalsSub: Subscription;
+  leftArrowIcon = faArrowCircleLeft;
+  paperPlaneIcon = faPaperPlane;
+  animal: { name: string; id: number | string } = null;
   subject: string = null;
-  id: string = null;
+  errorMessage: string = null;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  form = new FormGroup({
+    email: new FormControl(''),
+    subject: new FormControl(''),
+    content: new FormControl(''),
+  });
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private animalsService: AnimalsService,
+    private router: Router,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(params => {
-      if (params.animal) {
-        this.subject = `[INFO] ${params.animal}`;
-        this.id = params.id;
+    this.routeSub = this.activatedRoute.queryParams.subscribe((params) => {
+      if (params.animal && params.id) {
+        this.animal = { name: params.animal, id: params.id };
       }
     });
 
-    this.form = new FormGroup({
-      email: new FormControl(''),
-      subject: new FormControl({value: this.subject, disabled: this.subject}),
-      content: new FormControl('')
-    });
+    if (this.animal) {
+      this.animalsSub = this.animalsService
+        .fetchOneAnimal(+this.animal.id)
+        .subscribe(
+          (res: Animal) => {
+            if (this.animal.name !== res.name) {
+              this.setError(`l'animal ne correspond pas a l'id indiqué`);
+            }
+            this.subject = `[INFO] ${this.animal.name}`;
+            this.form.controls.subject.setValue(this.subject);
+            this.form.controls.subject.disable();
+          },
+          (err) => {
+            this.setError('Une erreur inconnue est survenue');
+          }
+        );
+    }
+  }
+
+  navigateToLastPage() {
+    this.location.back();
+  }
+
+  setError(msg: string) {
+    this.animal = null;
+    this.subject = null;
+    this.router.navigate(['/contact']);
+    this.errorMessage = msg;
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+    if (this.animalsSub) {
+      this.animalsSub.unsubscribe();
+    }
   }
 
   onSubmit() {
     // tslint:disable-next-line: forin
-    for (const control in this.form.value) {
-      console.log(`${control} : ${this.form.value[control]}`);
+    for (const [key, control] of Object.entries(this.form.controls)) {
+      console.log(`${key} : ${control.value}`);
     }
+    // console.log(this.form);
+  }
+
+  clearError() {
+    this.errorMessage = null;
   }
 }
