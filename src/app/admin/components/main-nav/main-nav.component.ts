5d53cd25f459b01497fcf821022c56eae68a89d6
Jester0027 <Paul.NED@outlook.com>
2020-05-13 11:08:03 +0200

    nav disconnect button + link fix

--- src/app/admin/components/main-nav/main-nav.component.ts
+++ src/app/admin/components/main-nav/main-nav.component.ts
@@ -1,5 +1,6 @@
+import { AuthService } from './../../services/auth.service';
 import { Title } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, AfterContentChecked } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -7,19 +8,31 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss']
+  styleUrls: ['./main-nav.component.scss'],
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit, AfterContentChecked {
   title: string;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private titleService: Title) {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private titleService: Title,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+  }
+  ngAfterContentChecked(): void {
     this.title = this.titleService.getTitle();
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
