14c6c1955a3d27fd448ccda990ee4affe3c46344
Jester0027 <Paul.NED@outlook.com>
2020-05-24 13:15:55 +0200

    refuges service pagination update

--- src/app/services/refuges.service.ts
+++ src/app/services/refuges.service.ts
@@ -1,6 +1,6 @@
 import { PaginatedData } from './../models/paginated-data.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from './../../environments/environment';
@@ -12,13 +12,18 @@ import { Refuge } from './../models/animals/refuge.model';
 export class RefugesService {
   constructor(private http: HttpClient) {}
 
-  fetchRefuges(): Observable<PaginatedData<Refuge>> {
-    return this.http.get<PaginatedData<Refuge>>(`${environment.api}/api/refuges`);
+  fetchRefuges(page: number = null): Observable<PaginatedData<Refuge>> {
+    const options = page
+      ? { params: new HttpParams().set('page', page.toString()) }
+      : {};
+
+    return this.http.get<PaginatedData<Refuge>>(
+      `${environment.api}/api/refuges`,
+      options
+    );
   }
 
   fetchOneRefuge(id: number): Observable<Refuge> {
-    return this.http.get<Refuge>(
-      `${environment.api}/api/refuges/${id}`
-    );
+    return this.http.get<Refuge>(`${environment.api}/api/refuges/${id}`);
   }
 }
